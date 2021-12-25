4ed025967a924ff1421c14b278b3204d5e95d5ba
maksimellian <maksimellian01@yandex.ru>
2021-12-25 06:31:24 +0300

    Scripted auto-commit on change (2021-12-25 06:31:23) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -1,11 +1,19 @@
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 public class TestingApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create();
         ActorRef actorCasher = system.actorOf(Props.create(ActorCasher.class), "cash");
-        
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(materializer, actorCasher;
     }
 }
