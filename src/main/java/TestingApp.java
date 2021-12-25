12b8e31d63eee1a7eea491c5a1c69ffcdf0901c9
maksimellian <maksimellian01@yandex.ru>
2021-12-25 06:25:56 +0300

    Scripted auto-commit on change (2021-12-25 06:25:56) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -5,6 +5,7 @@ import akka.actor.Props;
 public class TestingApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create();
-        ActorRef actorCasher = system.actorOf(Props.create(ActorCasher.class))
+        ActorRef actorCasher = system.actorOf(Props.create(ActorCasher.class), "cash");
+        
     }
 }
