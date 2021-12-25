5552e52631205d20db8fbd92c3ade713fd08facb
maksimellian <maksimellian01@yandex.ru>
2021-12-25 06:51:16 +0300

    Scripted auto-commit on change (2021-12-25 06:51:16) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -18,6 +18,7 @@ public class TestingApp {
     private static final String HOST = "localhost";
     private static final int PORT = 8080;
     private static final String URL = "connect";
+    private static final String COUNT = ""
 
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create();
