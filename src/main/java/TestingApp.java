dbd25e1ebf8f812a4bbff076b8e24106303959bf
maksimellian <maksimellian01@yandex.ru>
2021-12-25 06:33:46 +0300

    Scripted auto-commit on change (2021-12-25 06:33:46) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -14,6 +14,6 @@ public class TestingApp {
         ActorRef actorCasher = system.actorOf(Props.create(ActorCasher.class), "cash");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(materializer, actorCasher;
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(materializer, actorCasher);
     }
 }
