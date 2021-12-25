8d69914aff60706fd088699363355bc36144b546
maksimellian <maksimellian01@yandex.ru>
2021-12-25 06:45:01 +0300

    Scripted auto-commit on change (2021-12-25 06:45:01) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -29,6 +29,6 @@ public class TestingApp {
                 materializer
         );
         System.in.read();
-        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate())
+        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
     }
 }
