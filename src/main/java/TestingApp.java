1fd883b17e0c0471cd4b05a9b92e02a140331b44
maksimellian <maksimellian01@yandex.ru>
2021-12-25 06:46:24 +0300

    Scripted auto-commit on change (2021-12-25 06:46:24) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -33,5 +33,5 @@ public class TestingApp {
         binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
     }
 
-    private static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer)
+    private static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer materializer)
 }
