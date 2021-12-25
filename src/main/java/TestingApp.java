55d193e779078dcc1b2fc736b0ade5676ae1f7f9
maksimellian <maksimellian01@yandex.ru>
2021-12-25 07:12:18 +0300

    Scripted auto-commit on change (2021-12-25 07:12:18) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -63,7 +63,7 @@ public class TestingApp {
                                     });
                         })).map((r) -> {
                             casher.tell(new StoreMessage(r.first(), r.second()), ActorRef.noSender());
-                            return HttpResponse.create().withEntity("Result: ")
+                            return HttpResponse.create().withEntity("Result: " + r.)
                 })
         }
     }
