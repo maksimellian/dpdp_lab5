603cf8a65f88654e864fada30b703747e32fe6da
maksimellian <maksimellian01@yandex.ru>
2021-12-25 06:43:20 +0300

    Scripted auto-commit on change (2021-12-25 06:43:20) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -2,14 +2,19 @@ import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import java.util.concurrent.CompletionStage;
+
 public class TestingApp {
     private static final String HOST = "localhost";
+    private static final int PORT = 8080;
     private static final int
 
     public static void main(String[] args) {
