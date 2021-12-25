d316cbfc9f379db7bf7ea1546794cd6ee1c32832
maksimellian <maksimellian01@yandex.ru>
2021-12-25 06:48:11 +0300

    Scripted auto-commit on change (2021-12-25 06:48:11) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -38,7 +38,7 @@ public class TestingApp {
         return Flow.of(HttpRequest.class)
                 .map((r) -> {
                     Query query = r.getUri().query();
-                    String url = query
+                    String url = query.
                 })
         }
     }
